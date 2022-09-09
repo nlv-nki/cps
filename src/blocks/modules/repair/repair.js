0d8ec07c5b447389eb77457343a4530b8997afd4
Nikolai <nlv.nki@gmail.com>
2022-09-09 14:35:06 +0300

    block was added

--- src/blocks/modules/repair/repair.js
+++ src/blocks/modules/repair/repair.js
@@ -1,8 +1,18 @@
 import Swiper from 'swiper/bundle';
-import { items_box_shower } from '../../components/items-box/items-box';
+import { items_box_shower, breakpointChecker } from '../../components/items-box/items-box';
 let repair_container = document.querySelector('.repair__wrapper');
-let repair__btn = document.querySelector('.repair__btn');
+let repair__btn = document.querySelector('.repair__js-btn-shower');
+let repair__swiper_container = document.querySelector('.repair__swiper');
+let repair__swiper = undefined;
+
+const breakpoint = window.matchMedia('(min-width: 768px)');
 
 repair__btn.addEventListener('click', () => {
 	items_box_shower(repair_container, repair__btn);
+});
+
+breakpointChecker(repair__swiper_container, breakpoint);
+window.addEventListener('optimizedResize', () => {
+	console.log(123);
+	breakpointChecker(repair__swiper_container, breakpoint);
 });

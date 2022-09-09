b642b37126cd33503e1b231d70a8ae1ec80d01f1
Nikolai <nlv.nki@gmail.com>
2022-09-09 14:34:46 +0300

    added some func

--- src/blocks/components/items-box/items-box.js
+++ src/blocks/components/items-box/items-box.js
@@ -1,3 +1,4 @@
+import Swiper from 'swiper/bundle';
 (function () {
 	var throttle = function (type, name, obj) {
 		obj = obj || window;
@@ -23,13 +24,42 @@ const items_box_shower = (items_container, btn_shower) => {
 	if (!items_container.classList.contains('items-box__wrapper--expanded')) {
 		items_container.classList.add('items-box__wrapper--expanded');
 		btn_shower.innerText = 'Свернуть';
-		btn_shower.classList.add('link-more__btn--expanded');
+		btn_shower.classList.add('link-more--expanded');
 	} else {
 		items_container.classList.remove('items-box__wrapper--expanded');
 		items_container.classList.add('items-box__wrapper');
-		btn_shower.classList.remove('link-more__btn--expanded');
+		btn_shower.classList.remove('link-more--expanded');
 		btn_shower.innerText = 'Показать все';
 	}
 };
+let swiper;
+const breakpointChecker = function (swiper_container, breakpoint) {
+	console.log(breakpoint.matches);
+	//let swiper;
+	if (breakpoint.matches === true) {
+		console.log(swiper);
+		if (swiper !== undefined) swiper.destroy(true, true);
+		return;
+	} else if (breakpoint.matches === false) {
+		// fire small viewport version of swiper
+		console.log('breakpoint false' + ' swiper enable ');
+		swiper = enableSwiper(swiper_container);
+	}
+	//console.log(swiper);
+};
 
-export { items_box_shower };
+const enableSwiper = function (container) {
+	let swiper = new Swiper(container, {
+		slidesPerView: 1,
+		slidesPerView: 'auto',
+		spaceBetween: 15,
+		autoplay: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+	});
+	return swiper;
+};
+
+export { items_box_shower, enableSwiper, breakpointChecker };

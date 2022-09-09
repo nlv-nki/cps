cd541e7b5d922c9729042a4514d30c8f11b79fa5
Nikolai <nlv.nki@gmail.com>
2022-09-09 14:34:04 +0300

    brands section was implemented

--- src/blocks/modules/brands/brands.js
+++ src/blocks/modules/brands/brands.js
@@ -1,44 +1,45 @@
 import Swiper from 'swiper/bundle';
-import { items_box_shower } from '../../components/items-box/items-box';
+import { items_box_shower, breakpointChecker } from '../../components/items-box/items-box';
 let brands_container = document.querySelector('.brands__wrapper');
+let brands__swiper = undefined;
+let brands__swiper_container = document.querySelector('.brands__swiper');
 let brands__btn = document.querySelector('.brands__js-btn-shower');
+const breakpoint = window.matchMedia('(min-width: 768px)');
+
 brands__btn.addEventListener('click', () => {
 	items_box_shower(brands_container, brands__btn);
 });
 
-const breakpoint = window.matchMedia('(min-width: 768px)');
-let swiper;
+// const enableSwiper = function () {
+// 	let swiper;
+// 	swiper = new Swiper('.brands__swiper', {
+// 		slidesPerView: 1,
+// 		slidesPerView: 'auto',
+// 		spaceBetween: 15,
+// 		autoplay: true,
+// 		pagination: {
+// 			el: '.swiper-pagination',
+// 			clickable: true,
+// 		},
+// 	});
+// 	return swiper;
+// 	console.log('onn 1');
+// };
 
-const enableSwiper = function () {
-	swiper = new Swiper('.brands__swiper', {
-		slidesPerView: 1,
-		slidesPerView: 'auto',
-		spaceBetween: 15,
-		autoplay: true,
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-		},
-	});
+// const breakpointChecker = function () {
+// 	console.log(breakpoint.matches);
+// 	if (breakpoint.matches === true) {
+// 		if (swiper !== undefined) swiper.destroy(true, true);
+// 		return;
+// 	} else if (breakpoint.matches === false) {
+// 		// fire small viewport version of swiper
+// 		console.log('onn ');
 
-	console.log('onn 1');
-};
+// 		enableSwiper();
+// 	}
+// };
 
-const breakpointChecker = function () {
-	console.log(breakpoint.matches);
-	if (breakpoint.matches === true) {
-		if (swiper !== undefined) swiper.destroy(true, true);
-		return;
-	} else if (breakpoint.matches === false) {
-		// fire small viewport version of swiper
-		console.log('onn ');
-
-		enableSwiper();
-	}
-};
-
-breakpointChecker();
-
+breakpointChecker(brands__swiper_container, breakpoint);
 window.addEventListener('optimizedResize', () => {
-	breakpointChecker();
+	breakpointChecker(brands__swiper_container, breakpoint);
 });

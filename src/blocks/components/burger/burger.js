c4b0a7d71d1fe8053127c53af2ab6d9018615ca4
Nikolai <nlv.nki@gmail.com>
2022-09-15 15:55:44 +0300

    refactoring component

--- src/blocks/components/burger/burger.js
+++ src/blocks/components/burger/burger.js
@@ -1,11 +1,7 @@
+import { menu } from '../main-menu/main-menu';
 let burger_btn = document.querySelector('.burger__btn');
-let close_main_menu_btn;
-let menu = document.querySelector('.main-menu');
-let menu_close_btn = document.querySelector('.main-menu__btn-close');
+let overlay = document.querySelector('.overlay-cat');
 burger_btn.addEventListener('click', function (e) {
+	overlay.classList.add('active');
 	if (!menu.classList.contains('main-menu__js-is-active')) menu.classList.add('main-menu__js-is-active');
-});
-
-menu_close_btn.addEventListener('click', function (e) {
-	if (menu.classList.contains('main-menu__js-is-active')) menu.classList.remove('main-menu__js-is-active');
 });

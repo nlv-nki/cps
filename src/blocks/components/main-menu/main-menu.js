30d15bf1e680da148ec9d7a303fa068935373fdc
Nikolai <nlv.nki@gmail.com>
2022-09-18 15:51:13 +0300

    search component was implemented

--- src/blocks/components/main-menu/main-menu.js
+++ src/blocks/components/main-menu/main-menu.js
@@ -1,12 +1,20 @@
 import { modal } from '../modal-feedback/modal-feedback';
 let menu = document.querySelector('.main-menu');
+let menu__header = document.querySelector('.main-menu__header');
 let overlay = document.querySelector('.overlay-cat');
 let menu_close_btn = document.querySelector('.main-menu__btn-close');
 let main_menu_userlist = document.querySelector('.main-menu__user-list');
+let menu_search_btn = document.querySelector('.main-menu__btn-search');
 
 menu_close_btn.addEventListener('click', function (e) {
 	if (menu.classList.contains('main-menu__js-is-active')) menu.classList.remove('main-menu__js-is-active');
 	overlay.classList.remove('active');
+});
+
+menu_search_btn.addEventListener('click', function (e) {
+	if (!menu__header.classList.contains('main-menu__search-active')) {
+		menu__header.classList.add('main-menu__search-active');
+	} else menu__header.classList.remove('main-menu__search-active');
 });
 
 main_menu_userlist.addEventListener('click', (e) => {

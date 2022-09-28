0dca54d47283c24b1b66317e8316225edda2305d
Nikolai <nlv.nki@gmail.com>
2022-09-28 07:01:14 +0300

    rename export var

--- src/blocks/components/header-menu/header-menu.js
+++ src/blocks/components/header-menu/header-menu.js
@@ -1,11 +1,11 @@
-import { modal } from '../modal-feedback/modal-feedback';
+import { modal_feedback } from '../modal-feedback/modal-feedback';
 let overlay = document.querySelector('.overlay-cat');
 let header_menu_userlist = document.querySelector('.header-menu__user-list');
 header_menu_userlist.addEventListener('click', (e) => {
 	let target = e.target.closest('BUTTON');
 
 	if (target.classList.contains('header-menu__btn-modal')) {
-		modal.classList.add('modal-feedback__js-is-active');
+		modal_feedback.classList.add('modal-feedback__js-is-active');
 		overlay.classList.add('active');
 	}
 });

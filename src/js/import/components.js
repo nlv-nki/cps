24c959df738d3c10c622083a5945bfdb5ee09e86
Nikolai <nlv.nki@gmail.com>
2022-10-02 00:03:07 +0300

    del import

--- src/js/import/components.js
+++ src/js/import/components.js
@@ -1,5 +1,4 @@
 import '%components%/burger/burger';
 import '%components%/header-menu/header-menu';
-import '%components%/modal-feedback/modal-feedback';
 import '%components%/overlay-cat/overlay-cat';
 import '%components%/modalW/modalW';

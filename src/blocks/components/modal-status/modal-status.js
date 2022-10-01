5aaf4b5b1a6429877566929d7b89d02d2883cf29
Nikolai <nlv.nki@gmail.com>
2022-10-02 00:02:20 +0300

    added forms

---
+++ src/blocks/components/modal-status/modal-status.js
@@ -0,0 +1,13 @@
+modules.define('modal-status', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
